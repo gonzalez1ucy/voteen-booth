487e38cd53ac8b2240ee98e6b391d62ecc43dfcf
Luzmarie Gonzalez <glucy822@gmail.com>
2019-05-06 21:59:36 +0000

    saved

--- javascript.js
+++ javascript.js
@@ -1,3 +1,18 @@
 $("#Dem").click(function(){
     ("#Demo").show();
+    
+     
+});
+
+$("button").click(function() {
+    $.ajax({
+        url: "https://www.googleapis.com/civicinfo/v2/elections?key=<AIzaSyCB7viAXo9FP_4nN2Kh_4Qr0pcAuReT9Z8>",
+        method: "GET",
+        success: function (response) {
+       
+        }
+    }) 
+
+
+
 });
